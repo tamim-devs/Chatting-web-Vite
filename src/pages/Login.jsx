e4d836221fa3919fd7205463a56e493d0ea65a66
tamim-devs <tamimh0727@gmail.com>
2025-04-08 21:26:35 +0600

    Fix: Rename loginleft.jsx to LoginLeft.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,10 +3,12 @@ import LoginRight from '../components/Login/LoginRight'
 import LoginLeft from '../components/Login/LoginLeft'
 const Login = () => {
   return (
+    
     <div className='flex items-start'>
       <LoginLeft  />
       <LoginRight/>
     </div>
+
   )
 }
 
