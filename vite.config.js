65d245538fb9f622e5a760b5b9c10be2c82a567a
tamim-devs <tamimh0727@gmail.com>
2025-12-14 12:32:32 +0600

    first project

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  base: "/", // 🔥 MUST
+  base: "/", // ✅ MUST for Vercel
   plugins: [
     react(),
     VitePWA({
